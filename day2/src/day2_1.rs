c3120f9b96eded12d7f858f1232205404e38e2e7
Brady Law <bradyl16@gmail.com>
2021-12-01 19:51:40 -0800

    stuff

--- day2/src/day2_1.rs
+++ day2/src/day2_1.rs
@@ -1,21 +1,16 @@
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-use itertools::Itertools;
 
-fn main() -> io::Result<()>  {
+fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
-    let increase_count: u32 = reader.lines()
-        .map(|l| l.unwrap().parse::<u32>().expect("parse_error") )
+    let increase_count: u32 = reader
+        .lines()
+        .map(|l| l.unwrap().parse::<u32>().expect("parse_error"))
         .tuple_windows()
-        .map(|(a, b)| { 
-            if b > a {
-                1
-            } else {
-                0
-            }
-        })
+        .map(|(a, b)| if b > a { 1 } else { 0 })
         .sum();
 
     println!("Increase count: {}", increase_count);
