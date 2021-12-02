0515f1e411d6a7be3c9e40f7ce847b3b67503cc3
Brady Law <bradyl16@gmail.com>
2021-12-01 19:43:40 -0800

    progress

---
+++ day2/src/day2_1.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+use itertools::Itertools;
+
+fn main() -> io::Result<()>  {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    let increase_count: u32 = reader.lines()
+        .map(|l| l.unwrap().parse::<u32>().expect("parse_error") )
+        .tuple_windows()
+        .map(|(a, b)| { 
+            if b > a {
+                1
+            } else {
+                0
+            }
+        })
+        .sum();
+
+    println!("Increase count: {}", increase_count);
+
+    Ok(())
+}
